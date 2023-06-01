f8b789ca3a2627e75b330b74ee1e1fd245aaef2e
Robin Bühler <git+r@obin.ch>
2023-06-01 11:06:59 +0000

    chore: add task layout

---
+++ layouts/task.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps({
+  number: {
+    type: Number,
+  },
+  name: {
+    type: String
+  },
+  time: {
+    type: String
+  },
+  method: {
+    type: String
+  },
+  result: {
+    type: String
+  }
+})
+</script>
+
+<template>
+  <div class="slidev-layout default">
+    <div class="my-auto">
+      <TaskDescription :number="number" :title="name" :time="time" :method="method" :result="result" />
+      <slot />
+    </div>
+  </div>
+</template>
