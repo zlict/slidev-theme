60e5c185ef9ea6ddbb0e5f065f626110337dda2d
Robin Bühler <git+r@obin.ch>
2023-11-26 20:16:28 +0000

    feat: add slide numbers

---
+++ global-bottom.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer style="font-size: 0.5em" class="absolute top-7 right-4 pr-8 right-0 text-right text-gray-400 flex z-1">
+    <div>
+      <SlideCurrentNo />&nbsp;/&nbsp;<SlidesTotal />
+    </div>
+  </footer>
+</template>
