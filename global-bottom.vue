c3e7214cd72890c25713af9b19f0e12afc18fb02
Robin <git+r@obin.ch>
2026-03-02 14:51:43 +0000

    fix: page numbering on export

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,13 +1,13 @@
-<script setup>
-import { useNav } from '@slidev/client'
+<script setup lang="ts">
+import { useSlideContext } from '@slidev/client'
 
-const { currentPage, total } = useNav()
+const { $nav } = useSlideContext()
 </script>
 
 <template>
   <footer style="font-size: 0.5em" class="absolute top-7 right-4 pr-8 right-0 text-right text-gray-400 flex z-1">
     <div>
-      {{ currentPage }}&nbsp;/&nbsp;{{ total }}
+      {{ $nav.currentPage }}&nbsp;/&nbsp;{{ $nav.total }}
     </div>
   </footer>
 </template>
